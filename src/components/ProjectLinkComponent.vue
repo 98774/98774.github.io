a5ff2d2814ab9b19a19ff525504d060f07fe777d
Jonathon Shirk Snyder <j.snyder98774@gmail.com>
2024-03-05 16:30:49 -0500

    wip -- Project link successfully displays

---
+++ src/components/ProjectLinkComponent.vue
@@ -0,0 +1,76 @@
+<!-- BioComponent.vue -->
+
+<template>
+  <routerLink :to="routerLink">
+    <div class="container">
+      <div class="heading">
+        <div class="project-link">{{ linkText }}</div>
+      </div>
+      <div class="detail">
+        <slot name="details"></slot>
+      </div>
+    </div>
+  </routerLink>
+</template>
+
+<script lang="ts">
+import router from '@/router'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {}
+  },
+  props: {
+    routerLink: String,
+    linkText: String
+  }
+})
+</script>
+
+<style scoped>npm
+.project-link {
+  color: var(--accent-color-3);
+}
+
+.container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  height: 120px;
+  width: calc(0.8 * var(--content-max-width));
+}
+
+.detail {
+  flex-direction: column;
+  flex-grow: 1;
+  text-align: right;
+  margin: calc(3 *var(--default-margin));
+  overflow: hidden;
+  max-width: 0;
+  max-height: 100px;
+  transition: all 0.3s;
+}
+
+.container:hover {
+  border: 2px solid var(--highlight-color);
+  border-radius: 10px;
+  background-color: var(--highlight-background);
+  width: calc(var(--content-max-width) * 0.8);
+  cursor: pointer;
+}
+.container:hover .detail {
+  color: var(--accent-color-3);
+  max-width: calc(100% - 200px);
+  display: flex;
+}
+
+.container:hover .project-link {
+  background-color: var(--highlight-background);
+  font-weight: 999;
+  color: var(--highlight-color);
+}
+
+</style>
