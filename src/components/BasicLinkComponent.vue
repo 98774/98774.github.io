99f4a3dcdb8ccd466567669a613eda1b19266820
Jonathon Shirk Snyder <j.snyder98774@gmail.com>
2024-03-10 18:42:18 -0400

    [Update] ProgrammingTricks: Added basic component for representing links

---
+++ src/components/BasicLinkComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="horizontal-flex">
+    <p class="description">
+      <a target="_blank" rel="noopener noreferrer" :href="link">{{ name }}</a> -- {{ description }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {}
+  },
+  props: {
+    name: String,
+    link: String,
+    description: String
+  }
+})
+</script>
+
+<style scoped>
+.horizontal-flex {
+  justify-content: start;
+  align-items: center;
+  height: 40px;
+}
+</style>
--- src/views/ProgrammingTricksView.vue
+++ src/views/ProgrammingTricksView.vue
@@ -1,16 +1,35 @@
 <template>
   <div class="content">
-    <h1>Programming Tips and Tricks</h1>
+    <h1>Tips and Tricks</h1>
+    <p>This page aims to be a long-term source for useful technical tricks I pick up.</p>
+    <section class="content-panel">
+      <h2>Open-source Software:</h2>
+      <BasicLinkComponent
+        name="Neovim"
+        link="https://neovim.io/"
+        description="Neovim is a fork of Vim that aims to expand functionality"
+      />
+      <BasicLinkComponent
+        name="Neovim"
+        link="https://neovim.io/"
+        description="Neovim is a fork of Vim that aims to expand functionality"
+      />
+      <BasicLinkComponent
+        name="Neovim"
+        link="https://neovim.io/"
+        description="Neovim is a fork of Vim that aims to expand functionality"
+      />
+    </section>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
-import BioComponent from '@/components/BioComponent.vue' // Adjust the path based on your project structure
+import BasicLinkComponent from '@/components/BasicLinkComponent.vue'
 
 export default defineComponent({
   name: 'ProgrammingTricksView',
   components: {
-    BioComponent
+    BasicLinkComponent
   }
 })
 </script>
